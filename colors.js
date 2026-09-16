39f379051f8d1b4169f0e8aaadeed6e3e758d3bd
Ana <anavirgi2006@gmail.com>
2026-09-16 11:14:43 -0500

    update

--- colors.js
+++ colors.js
@@ -1,20 +1,19 @@
 function changeParagraph()
 {
-    var para = document.getElementbyID("change");
+    var para = document.getElementById("change");
 
-    var border_R = document.getElementbyID("border_R").value;
-    var border_G = document.getElementbyID("border_G").value;
-    var border_B = document.getElementbyID("border_B").value;
+    var border_R = document.getElementById("border_R").value;
+    var border_G = document.getElementById("border_G").value;
+    var border_B = document.getElementById("border_B").value;
     var border_width = document.getElementById("border_width").value;
 
-    var bgr_R = document.getElementbyID("bgr_R").value;
-    var bgr_G = document.getElementbyID("bgr_G").value;
-    var bgr_B = document.getElementbyID("bgr_B").value;
+    var bgr_R = document.getElementById("bgr_R").value;
+    var bgr_G = document.getElementById("bgr_G").value;
+    var bgr_B = document.getElementById("bgr_B").value;
 
 
     para.style.borderColor = `rgb(${border_R},${border_G},${border_B})`;
-    para.style.borderWidth = border_width;
+    para.style.borderWidth = `${border_width}px`;
     para.style.backgroundColor = `rgb(${bgr_R},${bgr_G},${bgr_B})`;
 
-    
 }
